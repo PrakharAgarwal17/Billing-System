851f7bd39e6425504dfd7cf18e69824543b9f64b
vibhu gupta <vibhugupta022@gmail.com>
2025-12-25 03:35:29 +0530

    done

--- Backend/config/Redis/redis.js
+++ Backend/config/Redis/redis.js
@@ -1,7 +1,7 @@
 import { createClient } from 'redis';
 const client = createClient({
     username: 'default',
-    password: 'LkAYaKnLd5zBqTFzV9AJHyLJ0124mYZy',
+    password: 'ok',
     socket: {
         host: 'redis-11382.c114.us-east-1-4.ec2.cloud.redislabs.com',
         port: 11382
